c8d4ea5ac0662efd646532216052e51e7fa707f3
Erin Kelley <erin.kelley01@gmail.com>
2024-10-02 15:56:11 -0600

    Add fire gif to images

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,25 @@
 import dayCamping from '../assets/day-camping.jpeg';
-import './Header.css'
+import nightCamping from '../assets/night-camping.jpeg';
+import './Header.css';
+import { useState } from 'react'
 
 export default function Header() {
+    const [checked, setChecked] = useState(false)
+
+    function handleClick() {
+        setChecked(!checked)
+    }
+    console.log(checked)
     return (
         <header>
-            <img class='camping-image' src={dayCamping} />
+
+            <img class='camping-image' src={checked ? dayCamping : nightCamping} />
+
+            <div class={checked ? 'fire' : 'nightFire'}>
+                <img class='camping-fire' src='https://media.baamboozle.com/uploads/images/1197896/1677692362_34951_gif-url.gif' />
+            </div>
+            <input type='checkbox' checked={checked} onClick={handleClick} />
+
         </header>
     )
 }
