858fc8018651f8f322581ba2cda5f5cd8c7ce8fc
Erin Kelley <erin.kelley01@gmail.com>
2024-10-02 17:01:30 -0600

    Add campfire audio

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,18 +1,40 @@
 import dayCamping from '../assets/day-camping.jpeg';
 import nightCamping from '../assets/night-camping.jpeg';
 import './Header.css';
-import { useState } from 'react'
+import { useState, useRef } from 'react'
+import sound from '../assets/campfire.mp3'
 
 export default function Header() {
     const [checked, setChecked] = useState(false)
-
+    const [audioChecked, setAudioChecked] = useState(false)
+    const myRef = useRef();
     function handleClick() {
         setChecked(!checked)
     }
-    console.log(checked)
-    return (
-        <header>
 
+
+    function startAudio() {
+        myRef.current.play();
+        setAudioChecked(true);
+    };
+
+    function pauseAudio() {
+        myRef.current.pause();
+        setAudioChecked(false);
+    };
+
+    return (
+
+        <header>
+            <audio loop
+                ref={myRef}
+                src={sound}
+            />
+            {audioChecked ? (
+                <button onClick={pauseAudio}>pause</button>
+            ) : (
+                <button onClick={startAudio}>start</button>
+            )}
             <img class='camping-image' src={checked ? dayCamping : nightCamping} />
 
             <div class={checked ? 'fire' : 'nightFire'}>
