56a94bbd1d68c753a47c04265ab3bd7f9fb0261b
Erin Kelley <erin.kelley01@gmail.com>
2024-10-03 13:39:13 -0600

    Add an error page

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css'
 import Header from './components/Header.tsx'
 import Gear from './components/Gear.tsx'
+import Error from './components/Error.tsx'
 function App() {
 
 
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Header />} />
         <Route path='/gear' element={<Gear />} />
+        <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
 
